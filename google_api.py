51fd89329d7de6cb78b91fe2564150ccf709c646
M_Saif_M <123909641+SaifOver9000@users.noreply.github.com>
2025-09-30 20:14:50 -0300

    Added documentation to the google api file

---
+++ google_api.py
@@ -0,0 +1,7 @@
+"""
+The purpose of this script is to handle the google calendar api calls. 
+
+It should be able to authenticate with the cloud and be able to handle the services requested
+
+"""
+import os
