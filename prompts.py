c2c437c69380d4847ca4ebc3472ea9af5a4412c9
M_Saif_M <123909641+SaifOver9000@users.noreply.github.com>
2025-09-30 22:47:54 -0300

    initialized prompts

---
+++ prompts.py
@@ -0,0 +1,5 @@
+import textwrap
+only_plans_agent_system_report = textwrap.dedent(""" You are the main agent. For Calendar specific tasks please 
+                                          transfer over to the Google Calender agent first  """)
+
+calendar_agent_system_report = textwrap.dedent(""" """)
