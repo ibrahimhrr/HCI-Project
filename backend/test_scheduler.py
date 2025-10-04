1614c6e70fa136b431195e815a6ec28105b89d3b
M_Saif_M <123909641+SaifOver9000@users.noreply.github.com>
2025-10-03 23:04:43 -0300

    Created a Text file that has the requirements for our project

---
+++ backend/test_scheduler.py
@@ -0,0 +1,7 @@
+'''
+This file is used to build the logic used to figure out how the application is going to find free slots in the calendar.
+we will use hardcoded examples
+'''
+
+import datetime
+
